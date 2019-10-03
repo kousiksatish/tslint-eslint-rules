b3dc8a0e67b0f1d9148599b6df80621020167376
Kousik Satish <kousiksatish@gmail.com>
2019-10-03 21:52:27 +0530

    Add tests and basic logic for rule

--- src/rules/terMaxParamsRule.ts
+++ src/rules/terMaxParamsRule.ts
@@ -2,26 +2,23 @@ import * as ts from 'typescript';
 import * as Lint from 'tslint';
 
 const RULE_NAME = 'ter-max-params';
-interface ITerMaxParamsOptions {
-  // Add the options properties
-}
 
 export class Rule extends Lint.Rules.AbstractRule {
   public static metadata: Lint.IRuleMetadata = {
     ruleName: RULE_NAME,
     hasFix: false,
-    description: '',
+    description: 'enforce a maximum number of parameters in function definitions',
     rationale: Lint.Utils.dedent`
+      Functions that take numerous parameters can be difficult to read and write because it
+      requires the memorization of what each parameter is, its type, and the order they should appear in.
+      As a result, many coders adhere to a convention that caps the number of parameters a function can take.
       `,
     optionsDescription: Lint.Utils.dedent`
       `,
     options: {
       type: 'array',
       items: [{
-        type: 'object',
-        properties: {
-        },
-        additionalProperties: false
+        type: 'number'
       }],
       maxLength: 1
     },
@@ -30,29 +27,38 @@ export class Rule extends Lint.Rules.AbstractRule {
         "${RULE_NAME}": [true]
         `,
       Lint.Utils.dedent`
-        "${RULE_NAME}": [true, {
-        }]
+        "${RULE_NAME}": [true, 2]
         `
     ],
     typescriptOnly: false,
     type: 'typescript'  // one of "functionality" | "maintainability" | "style" | "typescript"
   };
 
-  private formatOptions(ruleArguments: any[]): ITerMaxParamsOptions {
-    // handle the ruleArguments
-    return {};
-  }
+  public static FAILURE_STRING = 'function has too many parameters';
 
   public apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
-    // Convert the 'ruleArguments' into a useful format before passing it to the constructor of AbstractWalker.
-    const opt = this.formatOptions(this.ruleArguments);
-    const walker = new RuleWalker(sourceFile, this.ruleName, opt);
+    const walker = new MaxParamsRuleWalker(sourceFile, this.getOptions());
     return this.applyWithWalker(walker);
   }
 }
 
-// NOTE: please remove this comment after reading: https://palantir.github.io/tslint/develop/custom-rules/walker-design.html
-class RuleWalker extends Lint.AbstractWalker<ITerMaxParamsOptions> {
-  public walk(sourceFile: ts.SourceFile) {
+class MaxParamsRuleWalker extends Lint.RuleWalker {
+  protected visitArrowFunction(node: ts.ArrowFunction): void {
+    if (node.parameters.length > 2) {
+      this.getOptions().
+      this.addFailure(this.createFailure(node.getStart(), node.getEnd(), Rule.FAILURE_STRING));
+    }
+  }
+
+  protected visitMethodDeclaration(node: ts.MethodDeclaration): void {
+    if (node.parameters.length > 1) {
+      this.addFailure(this.createFailure(node.getStart(), node.getEnd(), Rule.FAILURE_STRING));
+    }
+  }
+
+  protected visitFunctionDeclaration(node: ts.FunctionDeclaration): void {
+    if (node.parameters.length > 1) {
+      this.addFailure(this.createFailure(node.getStart(), node.getEnd(), Rule.FAILURE_STRING));
+    }
   }
 }
--- src/test/rules/terMaxParamsRuleTests.ts
+++ src/test/rules/terMaxParamsRuleTests.ts
@@ -4,25 +4,35 @@ const ruleTester = new RuleTester('ter-max-params');
 
 // Change this function to better test the rule. In some cases the message never changes so we
 // can avoid passing it in. See other rule tests for examples.
-function expecting(errors: [string, number, number][]): Failure[] {
+function expecting(errors: [number, number][]): Failure[] {
   return errors.map((err) => {
-    let message = err[0];
+    let message = 'function has too many parameters';
     return {
       failure: message,
-      startPosition: new Position(err[1]),
-      endPosition: new Position(err[2])
+      startPosition: new Position(err[0]),
+      endPosition: new Position(err[1])
     };
   });
 }
 
-ruleTester.addTestGroup('group-name', 'should ...', [
+ruleTester.addTestGroup('no-options', 'should warn when the number of parameters exceeds the limit', [
+  {
+    code: ''
+  },
+  {
+    code: 'var x = 5;\nvar x = 2;'
+  },
+  {
+    code: 'var x = 5;\nvar x = 2;',
+    options: [2]
+  },
   {
     code: dedent`
-     // code goes here
+      function test(one: number, two: number, three: number) {
+        // code
+      }
      `,
-    options: [],
-    errors: expecting([
-    ])
+    errors: expecting([[1, 4]])
   }
 ]);
 
